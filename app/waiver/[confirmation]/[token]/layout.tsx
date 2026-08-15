4dfc3d4c6474ef1e4695063efde5d85d9a67c68e
Epic 4X4 Adventures <automation@epic4x4adventures.com>
2026-08-15 15:59:56 -0600

    Gate waiver pages by reservation business line

--- app/waiver/[confirmation]/[token]/layout.tsx
+++ app/waiver/[confirmation]/[token]/layout.tsx
@@ -1,5 +1,7 @@
 import "./waiver.css";
+import type { ReactNode } from "react";
+import WaiverBusinessLineGate from "./WaiverBusinessLineGate";
 
-export default function WaiverLayout({ children }: { children: React.ReactNode }) {
-  return children;
+export default function WaiverLayout({ children }: { children: ReactNode }) {
+  return <WaiverBusinessLineGate>{children}</WaiverBusinessLineGate>;
 }
