10f5a1bb835383710035ebc58be60e68b1189b0a
Epic 4X4 Adventures <automation@epic4x4adventures.com>
2026-08-20 16:32:07 -0600

    Mount dropped guest forms in reservation portal

--- app/guest/[token]/layout.tsx
+++ app/guest/[token]/layout.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from "react";
 import OhvUploadEnhancer from "./OhvUploadEnhancer";
 import ProtectionEnhancer from "./ProtectionEnhancer";
+import PortalGuestForms from "./PortalGuestForms";
 
 export default function GuestPortalLayout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
+      <PortalGuestForms />
       <OhvUploadEnhancer />
       <ProtectionEnhancer />
     </>
