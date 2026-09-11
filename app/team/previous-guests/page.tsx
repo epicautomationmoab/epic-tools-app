df6e4bdb7271a1428ef810581e389764639f0eb1
Epic 4X4 Adventures <automation@epic4x4adventures.com>
2026-09-11 10:20:25 -0600

    Remove redundant top navigation from Previous Guest Lookup

--- app/team/previous-guests/page.tsx
+++ app/team/previous-guests/page.tsx
@@ -1,9 +1,7 @@
-import Link from "next/link";
 import TeamSidebar from "../TeamSidebar";
 import HistoricalReadinessSearch from "../readiness/HistoricalReadinessSearch";
 import HistoricalPostVisitToggleEnhancer from "../readiness/HistoricalPostVisitToggleEnhancer";
 import HeaderClock from "../readiness/HeaderClock";
-import LogoutButton from "../readiness/LogoutButton";
 import styles from "../readiness/ReadinessShell.module.css";
 
 export default function PreviousGuestsPage() {
@@ -17,12 +15,6 @@ export default function PreviousGuestsPage() {
           <div className={styles.titleBlock}>
             <h1>Previous Guest Lookup</h1>
             <HeaderClock />
-          </div>
-          <div className={styles.headerActions}>
-            <Link className={styles.actionButton} href="/team/readiness">Guest Readiness</Link>
-            <Link className={styles.actionButton} href="/team/arrival-board">Arrival Board</Link>
-            <Link className={`${styles.actionButton} ${styles.kioskButton}`} href="/kiosk">Kiosk</Link>
-            <LogoutButton />
           </div>
         </header>
 
